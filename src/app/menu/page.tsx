a6c60c9308e4528e4ae37ab78da52c74e8f8842e
Mahmudd <mahmuddinudin00@gmail.com>
2026-06-25 11:18:30 +0700

    update terbaru

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -403,7 +403,9 @@ export default function MenuPage() {
                     // MODE KONFIRMASI HAPUS INLINE
                     <div className="flex items-center gap-1 flex-1">
                       <span className="text-xs text-red-400">
-                        Hapus &ldquo;{k.nama}&rdquo;?
+                        Hapus {'"'}
+                        {k.nama}
+                        {'"'}?
                       </span>
                       <button
                         onClick={() => executeDeleteKat(k.id)}
